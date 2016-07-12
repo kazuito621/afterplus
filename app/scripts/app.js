010aa04540a885e504162ef7bad6dc8c0616a1d8
NewLeaf <newleaf@NewLeafs-Mac.local>
2016-07-12 11:24:11 +0900

    initial
    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoNgApp
+ * @description
+ * # yoNgApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoNgApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoNgApp
+ */
+angular.module('yoNgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
